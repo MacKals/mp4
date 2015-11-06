a0c0527e346d3be82f56a25226b6c2d6e495c35b
mKals <m.kals@me.com>
2015-11-05 19:09:48 -0800

    removed interfaces

--- src/ca/ubc/ece/cpen221/mp4/items/animals/ArenaAnimal.java
+++ src/ca/ubc/ece/cpen221/mp4/items/animals/ArenaAnimal.java
@@ -1,25 +1,37 @@
 package ca.ubc.ece.cpen221.mp4.items.animals;
 
-import javax.swing.ImageIcon;
-
 import ca.ubc.ece.cpen221.mp4.Actor;
 import ca.ubc.ece.cpen221.mp4.Item;
-import ca.ubc.ece.cpen221.mp4.Location;
-import ca.ubc.ece.cpen221.mp4.Util;
-import ca.ubc.ece.cpen221.mp4.World;
-import ca.ubc.ece.cpen221.mp4.commands.Command;
 
-public abstract class ArenaAnimal extends Actor implements ArenaAnimalInterface { 
+
+
+/**
+ * Abstractions for foxes and rabbits that provide additional information that
+ * might be of use to the AI:
+ * <ol>
+ * <li>Maximum energy, given by {@link #getMaxEnergy()}</li>
+ * <li>View range, given by {@link #getViewRange()}</li>
+ * <li>Minimum breeding energy, given by {@link #getMinimumBreedingEnergy()}
+ * </li>
+ * </ol>
+ */
+public abstract class ArenaAnimal extends Actor { 
     
-    
+
     protected int MIN_BREEDING_ENERGY;
 
-    @Override
+    /**
+     * Returns the minimum energy required for an animal to breed
+     *
+     * @return the minimum breeding energy of this animal
+     */
     public int getMinimumBreedingEnergy() {
         return MIN_BREEDING_ENERGY;
     }
 
-    @Override
+    /**
+     * TODO: implement comment
+     */
     public void attack(Item enemy) {
         if ((this.MAX_ENERGY - this.energy) > enemy.getEnergy()){
             this.energy += enemy.getEnergy(); 
@@ -29,7 +41,21 @@ public abstract class ArenaAnimal extends Actor implements ArenaAnimalInterface 
         }
     }
 
-    @Override
+    /**
+     * Breeds a child of this {@link ArenaAnimal}, the child must be the same
+     * animal as the parent. A {@link ArenaAnimal} can only breed when all of the
+     * following conditions are satisfied:
+     * <ol>
+     * <li>There is an empty location adjacent to its location</li>
+     * <li>If it is an {@link ArenaAnimal}, its energy level must exceeds its
+     * breeding limit, specified by
+     * {@link ArenaAnimal#getMinimumBreedingEnergy()}</li>
+     * </ol>
+     * After breeding, both the parent and the child should have half of the
+     * energy of the parent's energy.
+     *
+     * @return child the offspring of this animal
+     */
     public abstract ArenaAnimal breed();
 
 }
